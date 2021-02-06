d267d5615240211caa71cdd9013237c361f1b44e
Steve Greenley <Steve.Greenley@gmail.com>
2021-02-06 15:11:13 +1300

    Added map overlay

--- src/screens/map-screen.js
+++ src/screens/map-screen.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
 import FocusAwareStatusBar from '../components/focus-aware-status-bar';
+import { SafeAreaView, Alert } from 'react-native';
+import { Button, SearchBar } from 'react-native-elements';
 
 const mapStyle = require('../../assets/custom-map-style.json');
 
 const MapScreen = () => {
+  const [searchValue, setSearchValue] = useState('');
   return (
     <>
       <FocusAwareStatusBar barStyle="dark-content" />
@@ -12,26 +15,63 @@ const MapScreen = () => {
         provider="google"
         style={styles.container}
         showsUserLocation
-        showsScale
-        showsCompass
         showsMyLocationButton
         customMapStyle={mapStyle}
       />
+
+      <SafeAreaView
+        style={[styles.overlay,styles.content]}
+        pointerEvents="box-none"
+      >
+        <SearchBar
+          placeholder="Search"
+          returnKeyType="search"
+          value={searchValue}
+          lightTheme
+          inputContainerStyle={styles.inputContainer}
+          containerStyle={styles.searchBarContainer}
+          onChangeText={(text)=>{
+            setSearchValue(text);
+          }}
+          onSubmitEditing={()=>{
+            Alert.alert('Search value submitted');
+          }}
+        />
+        <Button
+          title="Press me"
+          onPress={()=>{
+            Alert.alert('Pressed!');
+          }}
+        />
+      </SafeAreaView>
     </>
   );
 };
 
 const styles = {
   container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flex: 1
   },
   content: {
-    backgroundColor: 'hotpink',
-    padding: 30,
-    margin: 10,
-    borderRadius: 10
+    flex: 1,
+    justifyContent:'space-between',
+    alignItems: 'center',
+    margin: 20
+  },
+  overlay: {
+    position: 'absolute',
+    left: 0, right: 0,
+    top: 0, bottom: 0,
+  },
+  searchBarContainer: {
+    width: '100%',
+    backgroundColor: 'transparent',
+    borderTopWidth: 0,
+    borderBottomWidth: 0
+  },
+  inputContainer: {
+    borderRadius: 30,
+    backgroundColor:'rgba(255,255,255,0.8)'
   }
 };
 
