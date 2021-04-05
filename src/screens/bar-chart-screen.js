c5c2e4444b87c76c319e5481104d88e0dfd368f0
Steve Greenley <Steve.Greenley@gmail.com>
2021-04-05 20:53:00 +1200

    Added auto scaling to bar chart screen

--- src/screens/bar-chart-screen.js
+++ src/screens/bar-chart-screen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, LayoutAnimation } from 'react-native';
 import BarGraph from '../../components/bar-graph';
 import { weeklyData } from '../../lib/utils';
 
@@ -9,12 +9,25 @@ const dummyWeeklyData = weeklyData
     usageDollars: Math.round(Math.random() * 7000) / 100
   }));
 
+const maxDollarValue = dummyWeeklyData
+  .map(i=>i.usageDollars)
+  .reduce((acc,val)=>Math.max(acc,val));
+
 const BarChartScreen = (props) => {
+  const [containerHeight, setContainerHeight] = useState(1);
+  const scaleFactor = 0.8 * containerHeight / maxDollarValue;
+  console.log('!!! scaleFactor', scaleFactor);
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      onLayout={({ nativeEvent: { layout: { height } } })=>{
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setContainerHeight(height);
+      }}
+    >
       <BarGraph
         data={dummyWeeklyData}
-        scaleFn={item => (item.usageDollars * 8)}
+        scaleFn={item => (item.usageDollars * scaleFactor)}
       />
     </View>
   );
